2ded8d50732f5ba4d82dc78576812877c9f35a4b
Aditi Menon <aditimenon@Aditis-MacBook-Pro.local>
2026-01-27 15:12:41 -0800

    Edited file

--- lab01-part-a.cpp
+++ lab01-part-a.cpp
@@ -1,7 +1,7 @@
-#include <cstddef>
+#include <cstddef> // 
 #include <iostream>
 #include <string>
-#include <limits>
+#include <limits> // for clearing input buffer after cin
 
 using namespace std;
 
@@ -26,24 +26,24 @@ void printFoods(string*, const unsigned);
 
 int main() {
     // variables
-    string*  dynArr   = nullptr;
-    unsigned userSize = 0;
+    string*  dynArr   = nullptr; // Pointer that will later point to a dynamically alloacted array of strings
+    unsigned userSize = 0; // Stores the size of the array chosen by the user 
 
     // get the size for the array from user
     // validation: must not exceed 10
     do {
-        cout << "Enter your desired array size: ";
-        cin >> userSize;
-    } while (userSize > 10);
+        cout << "Enter your desired array size: "; // Asks the user for an array size
+        cin >> userSize; 
+    } while (userSize > 10); // This part makes sure that the size of the array is in between 1 and 10
 
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the leftover newline so that getline can work properly... mixing cin and getline without this would make the program skip the first input line
 
     // TODO: create the dynamic memory
-    dynArr = new string[userSize]; 
+    dynArr = new string[userSize];// assigns dynArr to point to the first element of the array which the user inputted 
 
     // call the functions
-    populate(dynArr, userSize);
-    printFoods(dynArr, userSize);
+    populate(dynArr, userSize); // Fills the array with the user's input
+    printFoods(dynArr, userSize); // Prints the food orders and their memory addresses 
 
     // TODO: release the dynamic memory to avoid a memory leak
     delete[] dynArr; 
@@ -66,10 +66,13 @@ int main() {
  * N/A
 *******************************************************************************/
 
+// This functions fills the dynamic array with food orders entered by the user 
+// arrPtr is a pointer to the first element of the dynamically allocated array
+// ARR_SIZE represents how many elements the array contains 
 void populate(string* arrPtr, const unsigned ARR_SIZE) {
-    for (unsigned i = 0; i < ARR_SIZE; ++i) {
-        cout << "\nEnter food order #" << (i + 1) << ": "; 
-        getline(cin, arrPtr[i]); // sotres the input into the i-th element 
+    for (unsigned i = 0; i < ARR_SIZE; ++i) { // Loops through each position in the array 
+        cout << "\nEnter food order #" << (i + 1) << ": "; // (i + 1) is used so the output starts counting from 1 instead of 0
+        getline(cin, arrPtr[i]); // uses array indexing to access elements in the array 
     }
 }
 
@@ -87,14 +90,16 @@ void populate(string* arrPtr, const unsigned ARR_SIZE) {
  * N/A
 *******************************************************************************/
 
+// This function print each food order stored in the dynamic array 
+// and print the memory address where each element is stored 
 void printFoods(string* arrPtr, const unsigned ARR_SIZE) {
     cout << endl; // space between last input and first list 
-    for (unsigned i = 0; i < ARR_SIZE; ++i) {
+    for (unsigned i = 0; i < ARR_SIZE; ++i) { // loops through each element in the array
         cout << "****************************************" << endl; 
-        cout << "Food Order #" << (i + 1) << endl; 
-        cout << arrPtr[i] << endl; 
+        cout << "Food Order #" << (i + 1) << endl; // It first prints the food order number starting from 1 
+        cout << arrPtr[i] << endl; // Prints the food order stored at the i-th position in the array (array-indexing)
         cout << "(sent from address " 
-             << static_cast<const void*>(&arrPtr[i]) 
+             << static_cast<const void*>(&arrPtr[i]) // static_cast makes sure the exact location in memory is printed 
              << ")" << endl; 
         cout << "****************************************" << endl;
         cout << endl; 
